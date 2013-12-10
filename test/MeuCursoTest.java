faebcb519588d04a384f40b224c7b6cd85393e38
Nazareno <nazareno@computacao.ufcg.edu.br>
2013-12-10 18:49:24 -0300

    Primeiros comportamentos pensados

---
+++ test/MeuCursoTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MeuCursoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void deveIniciarComPrimeiroPeriodoAlocado() {
+		
+	}
+	
+	@Test
+	public void deveAtualizarAlocacaoAoAdicionar() {
+	}
+	
+	@Test
+	public void deveAtualizarAlocacaoAoRemover() {
+	}
+	
+	@Test
+	public void deveRespeitarPrerequisitos() {
+	}
+	
+	@Test
+	public void deveRespeitarLimiteDeCreditos() {
+	}
+
+}
