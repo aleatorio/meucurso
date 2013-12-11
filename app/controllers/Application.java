15957c2d9783e7ebb848180be7f8aa9770519c9c
Nazareno <nazareno@computacao.ufcg.edu.br>
2013-12-10 22:42:03 -0300

    Inacreditavel, funciona !

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,14 @@
 package controllers;
 
+import models.Planejador;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(views.html.index.render(""));
-    }
->>>>>>> 8424327e55fb0ea602db53f994b582653074745e
+	public static final Planejador planejador = new Planejador();
+
+	public static Result index() {
+		return ok(views.html.index.render(planejador.getPeriodos()));
+	}
 }
