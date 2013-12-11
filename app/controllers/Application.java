46cde1dab427ec12c8092786970b29f4b38ba15d
Nazareno <nazareno@computacao.ufcg.edu.br>
2013-12-10 21:59:24 -0300

    Minimo de interface no ar

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,16 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import models.Planejador;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	private static Planejador planejador = new Planejador();
+
+	public static Result index() {
+		return ok(index.render(planejador.getPeriodos()));
+	}
 
 }
